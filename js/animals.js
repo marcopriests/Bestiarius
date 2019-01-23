af838be3ea6ac51aa8ad5737252afb1383ef02ec
marcopriests <prtmrc98@gmail.com>
2019-01-23 14:52:51 +0100

    Aggiunti: autocomplete e bottone disabilitato quando l'input è vuoto

--- js/animals.js
+++ js/animals.js
@@ -9,13 +9,22 @@ let estinto;
 let rischio;
 let recupero;
 let fpericolo;
+let nomi = [];
 
+$.getJSON("js/animalia.json", function (data) {
+    console.log(data.animalia)
+    for (let i = 0; i < data.animalia.length; i++) {
+        nomi.push(data.animalia[i].commonName);
+    }
+    console.log(nomi)
+});
 
 
 $("#button").click(function () {
 
     $.getJSON("js/animalia.json", function (data) {
         console.log(data.animalia)
+
         for (let i = 0; i < data.animalia.length; i++) {
             //console.log(data.animalia[i].commonName)
             if (data.animalia[i].commonName.includes(inputValue.value)) {
@@ -28,22 +37,22 @@ $("#button").click(function () {
                     readMore: data.animalia[i].readMore
                 }
 
-                if (data.animalia[i].alertStatus == "Estinto"){
+                if (data.animalia[i].alertStatus == "Estinto") {
                     estinto = 100;
                     rischio = 0;
                     recupero = 0;
                     fpericolo = 0;
-                } else if (data.animalia[i].alertStatus == "A Rischio"){
+                } else if (data.animalia[i].alertStatus == "A Rischio") {
                     estinto = 0;
                     rischio = 100;
                     recupero = 0;
                     fpericolo = 0;
-                } else if (data.animalia[i].alertStatus == "In Recupero"){
+                } else if (data.animalia[i].alertStatus == "In Recupero") {
                     estinto = 0;
                     rischio = 0;
                     recupero = 100;
                     fpericolo = 0;
-                } else if (data.animalia[i].alertStatus == "Fuori Pericolo"){
+                } else if (data.animalia[i].alertStatus == "Fuori Pericolo") {
                     estinto = 0;
                     rischio = 0;
                     recupero = 0;
@@ -126,3 +135,121 @@ $("#button").click(function () {
     barChart.update;
     console.log(barChart.data.datasets[0].data)
 }*/
+
+
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function (e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false; }
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+            /*check if the item starts with the same letters as the text field value:*/
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function (e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
+document.getElementById("button").classList.add("disabled")
+$("#input").keyup(function(){
+    if (document.getElementById("input").value == "") {
+        document.getElementById("button").classList.add("disabled")
+    } else {
+        document.getElementById("button").classList.remove("disabled")
+    }
+})
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function (event) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Trigger the button element with a click
+        document.getElementById("button").click();
+    }
+}); 
