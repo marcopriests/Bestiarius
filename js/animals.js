9527252119408e9e633f05aba7631678da0a7b6d
marcopriests <prtmrc98@gmail.com>
2019-01-25 16:22:07 +0100

    Aggiunte finali e loader

--- js/animals.js
+++ js/animals.js
@@ -22,117 +22,128 @@ $.getJSON("js/animalia.json", function (data) {
 
 $("#button").click(function () {
 
-    $(".transform").addClass("transform-active");
-    $(".logoNbar").addClass("logoNbar-active");
-    $("#container").fadeIn("slow");
-    $("#container").css("display", "flex");
+    $("#loader").css("display", "block");
 
-    $.getJSON("js/animalia.json", function (data) {
-        console.log(data.animalia)
-
-        for (let i = 0; i < data.animalia.length; i++) {
-            //console.log(data.animalia[i].commonName)
-            if (data.animalia[i].commonName.includes(inputValue.value)) {
-                context = {
-                    commonName: data.animalia[i].commonName,
-                    latinName: data.animalia[i].latinName,
-                    description: data.animalia[i].description,
-                    alertStatus: data.animalia[i].alertStatus,
-                    image: data.animalia[i].image,
-                    readMore: data.animalia[i].readMore
-                }
-
-                if (data.animalia[i].alertStatus == "Estinto") {
-                    estinto = 100;
-                    rischio = 0;
-                    recupero = 0;
-                    fpericolo = 0;
-                } else if (data.animalia[i].alertStatus == "A Rischio") {
-                    estinto = 0;
-                    rischio = 100;
-                    recupero = 0;
-                    fpericolo = 0;
-                } else if (data.animalia[i].alertStatus == "In Recupero") {
-                    estinto = 0;
-                    rischio = 0;
-                    recupero = 100;
-                    fpericolo = 0;
-                } else if (data.animalia[i].alertStatus == "Fuori Pericolo") {
-                    estinto = 0;
-                    rischio = 0;
-                    recupero = 0;
-                    fpericolo = 100;
-                }
+    let card = function () {
 
 
-                //console.log(data.animalia[i])
-                el_html = template(context);
-                $("#animalCard").html(el_html);
+        $(".transform").addClass("transform-active");
+        $(".logoNbar").addClass("logoNbar-active");
+        $("#container").fadeIn("slow");
+        $("#container").css("display", "flex");
+
+        $.getJSON("js/animalia.json", function (data) {
+            console.log(data.animalia)
 
 
-                let barChart = new Chart(CHART, {
-                    type: 'bar',
-                    data: {
-                        labels: [],
-                        datasets: [
-                            {
-                                label: 'estinto',
-                                data: [estinto, 0],
-                                backgroundColor: "#444444"
-                            },
-                            {
-                                label: 'a rischio',
-                                data: [rischio, 0],
-                                backgroundColor: "#f26868"
-                            },
-                            {
-                                label: 'in recupero',
-                                data: [recupero, 0],
-                                backgroundColor: "#f1c867"
-                            },
-                            {
-                                label: 'fuori pericolo',
-                                data: [fpericolo, 0],
-                                backgroundColor: "#428a3c"
-                            }
-                        ]
-                    },
-                    options: {
-                        legend: {
-                            display: true,
-                            position: "bottom"
-                        },
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    display: false
-                                },
-                                gridLines: {
-                                    display: false,
-                                    drawBorder:  false
-                                }
-                            }],
-                            xAxes: [{
-                                ticks: {
-                                    display: false
-                                },
-                                gridLines: {
-                                    display: false,
-                                }
-                            }]
+            $("#loader").css("display", "none");
 
-                        },
+            for (let i = 0; i < data.animalia.length; i++) {
+                //console.log(data.animalia[i].commonName)
+                if (data.animalia[i].commonName.includes(inputValue.value)) {
+                    context = {
+                        commonName: data.animalia[i].commonName,
+                        latinName: data.animalia[i].latinName,
+                        description: data.animalia[i].description,
+                        alertStatus: data.animalia[i].alertStatus,
+                        image: data.animalia[i].image,
+                        readMore: data.animalia[i].readMore
                     }
-                });
 
-                barChart.update()
+                    if (data.animalia[i].alertStatus == "Estinto") {
+                        estinto = 100;
+                        rischio = 0;
+                        recupero = 0;
+                        fpericolo = 0;
+                    } else if (data.animalia[i].alertStatus == "A Rischio") {
+                        estinto = 0;
+                        rischio = 100;
+                        recupero = 0;
+                        fpericolo = 0;
+                    } else if (data.animalia[i].alertStatus == "In Recupero") {
+                        estinto = 0;
+                        rischio = 0;
+                        recupero = 100;
+                        fpericolo = 0;
+                    } else if (data.animalia[i].alertStatus == "Fuori Pericolo") {
+                        estinto = 0;
+                        rischio = 0;
+                        recupero = 0;
+                        fpericolo = 100;
+                    }
 
-                barChart.canvas.parentNode.style.height = '200px';
+
+                    //console.log(data.animalia[i])
+                    el_html = template(context);
+                    $("#animalCard").html(el_html);
+
+
+                    let barChart = new Chart(CHART, {
+                        type: 'bar',
+                        data: {
+                            labels: [],
+                            datasets: [
+                                {
+                                    label: 'estinto',
+                                    data: [estinto, 0],
+                                    backgroundColor: "#444444"
+                                },
+                                {
+                                    label: 'a rischio',
+                                    data: [rischio, 0],
+                                    backgroundColor: "#f26868"
+                                },
+                                {
+                                    label: 'in recupero',
+                                    data: [recupero, 0],
+                                    backgroundColor: "#f1c867"
+                                },
+                                {
+                                    label: 'fuori pericolo',
+                                    data: [fpericolo, 0],
+                                    backgroundColor: "#428a3c"
+                                }
+                            ]
+                        },
+                        options: {
+                            legend: {
+                                display: true,
+                                position: "bottom"
+                            },
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        display: false
+                                    },
+                                    gridLines: {
+                                        display: false,
+                                        drawBorder: false
+                                    }
+                                }],
+                                xAxes: [{
+                                    ticks: {
+                                        display: false
+                                    },
+                                    gridLines: {
+                                        display: false,
+                                    }
+                                }]
+
+                            },
+                        }
+                    });
+
+                    barChart.update()
+
+                    barChart.canvas.parentNode.style.height = '200px';
+                }
             }
-        }
 
-    });
+        });
+    }
+    setTimeout(card, 1000);
 })
+
 
 /*function updateChart() {
     barChart.data.datasets[0].data = [o, 0];
@@ -239,7 +250,7 @@ function autocomplete(inp, arr) {
 }
 
 document.getElementById("button").classList.add("disabled")
-$("#input").keyup(function(){
+$("#input").keyup(function () {
     if (document.getElementById("input").value == "") {
         document.getElementById("button").classList.add("disabled")
     } else {
